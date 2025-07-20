99f49bf445723d63a2ddc5d2a4b0bdb384293055
Florin Romulescu <romulescu.florin.dev@gmail.com>
2025-07-20 14:06:23 +0300

    Remove start_xboard.sh script as it is no longer needed for XBoard integration with ChessBot engine.

--- scripts/start_xboard.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Script to start XBoard with ChessBot engine
-
-echo "Starting XBoard with ChessBot engine..."
-echo "Engine path: /home/florinrm/chessbot/scripts/run_uci_engine.py"
-echo ""
-
-# Start XBoard with engine configuration
-xboard -fcp "uv run python3 /home/florinrm/chessbot/scripts/run_uci_engine.py" -fd "/home/florinrm/chessbot" 
